3c01f0f70c9e3a3b2530935d0ddc5a60758ebdb2
Rohit Kumar <rohit9075@gmail.com>
2018-09-28 12:56:36 +0530

    final fragment java code to send the data Activity to fragment using fragment method

--- app/src/main/java/com/rohit/senddatatofragmentusingmethod/MainActivity.java
+++ app/src/main/java/com/rohit/senddatatofragmentusingmethod/MainActivity.java
@@ -2,12 +2,38 @@ package com.rohit.senddatatofragmentusingmethod;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mEmailEditText;
+    Button mSendDataButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEmailEditText = findViewById(R.id.email);
+        mSendDataButton = findViewById(R.id.button_send_data);
+
+        mSendDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String getEamil = mEmailEditText.getText().toString();
+
+                ReceiverFragment fragment = new ReceiverFragment();
+
+                fragment.sendData(getEamil);
+
+                getSupportFragmentManager().beginTransaction().add(R.id.container, fragment,"ReceiverFragment").commit();
+
+
+            }
+        });
     }
 }
--- app/src/main/java/com/rohit/senddatatofragmentusingmethod/ReceiverFragment.java
+++ app/src/main/java/com/rohit/senddatatofragmentusingmethod/ReceiverFragment.java
@@ -6,12 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ReceiverFragment extends Fragment {
+
+    private String email;
 
 
     public ReceiverFragment() {
@@ -23,7 +29,25 @@ public class ReceiverFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_receiver, container, false);
+        View view =  inflater.inflate(R.layout.fragment_receiver, container, false);
+
+        final TextView mTextView = view.findViewById(R.id.received_email);
+        Button mButton = view.findViewById(R.id.show_email);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextView.setText(email);
+            }
+        });
+
+
+        return view;
+    }
+
+
+    public void sendData(String email){
+        this.email = email;
     }
 
 }
